8b35635d56db73c08b595032d81010a812486a0c
Joshi <rajeshjoshi7860@gmail.com>
2021-06-11 14:45:56 +0000

    Highest Wicket Taker

---
+++ Module1/6-WebScrapping/Activity-HighestWicketTaker/highestWicketTaker.js
@@ -0,0 +1,20 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+let link = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
+
+request(link,cb);
+
+
+function cb(error,response,html){
+    eval(html);
+}
+
+
+function eval(html){
+    let ch = cheerio.load(html);
+
+    let winningTeam = ch(".match-header .status-text span").text();
+
+    console.log(winningTeam);
+}
