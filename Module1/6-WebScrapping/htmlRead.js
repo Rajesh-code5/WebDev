5b64575916e7de46e443873598a40e96a41070e3
Joshi <rajeshjoshi7860@gmail.com>
2021-06-11 14:23:18 +0000

    cheerio used

--- Module1/6-WebScrapping/htmlRead.js
+++ Module1/6-WebScrapping/htmlRead.js
@@ -6,4 +6,8 @@ let htmlData = fs.readFileSync("./index.html", "utf-8");
 
 let data = cheerio.load(htmlData);
 
-console.log(data);
+// console.log(data);
+
+let ch = data("body>p").text();
+
+console.log(ch);
