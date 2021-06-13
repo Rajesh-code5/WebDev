470a6832d1b0a7649ff425fffe9426f8e6d5f99f
Joshi <rajeshjoshi7860@gmail.com>
2021-06-13 23:56:18 +0000

    Extensions

---
+++ Module1/4-FolderSorting/extensions.js
@@ -0,0 +1,10 @@
+let extensions = {
+    "Audio" : ["mp3","flac","wav"],
+    "Compressed" : ["zip"],
+    "Documents" : ["pdf","xlsx","doc","docx","doc","csv","xml"],
+    "Images" : ["jpg","jpeg","gif","png"],
+    "Videos" : ["mp4","avi"]
+};
+
+
+module.exports = extensions;
